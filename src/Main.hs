f6397899d02f173e5e48db3a5d1c23912f82be40
Olle Fredriksson <fredriksson.olle@gmail.com>
2020-12-25 23:02:32 +0100

    🌋

---
+++ src/Main.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+module Main where
+
+import Control.DeepSeq (NFData)
+import Data.HashMap.Lazy (HashMap)
+import qualified Data.HashMap.Lazy as HashMap
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import qualified Data.List as List
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Proxy (Proxy(Proxy))
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import qualified Gauge
+import Prelude hiding (lookup)
+
+newtype Element = Element Int
+  deriving NFData
+
+class Environment env where
+  name :: String
+  empty :: env
+  extend :: Element -> env -> env
+  lookup :: env -> Int -> Element
+
+instance Environment [Element] where
+  name = "Data.List"
+  empty = []
+  extend = (:)
+  lookup = (List.!!)
+
+instance Environment (Map Int Element) where
+  name = "Data.Map"
+  empty = mempty
+  extend x m = Map.insert (Map.size m) x m
+  lookup m i = m Map.! (Map.size m - i - 1)
+
+instance Environment (Int, IntMap Element) where
+  name = "Data.IntMap"
+  empty = (0, mempty)
+  extend x (!size, !m) = (1 + size, IntMap.insert size x m)
+  lookup (size, m) i = m IntMap.! (size - i - 1)
+
+instance Environment (Int, HashMap Int Element) where
+  name = "Data.HashMap.Lazy"
+  empty = (0, mempty)
+  extend x (!size, !m) = (1 + size, HashMap.insert size x m)
+  lookup (size, m) i = m HashMap.! (size - i - 1)
+
+instance Environment (Seq Element) where
+  name = "Data.Sequence"
+  empty = mempty
+  extend = (Seq.<|)
+  lookup = Seq.index
+
+fromList :: (Environment env) => [Element] -> env
+fromList = foldr extend empty
+
+withEnvironmentTypes
+  :: forall result
+  . (forall env. (Environment env, NFData env) => Proxy env -> result)
+  -> [result]
+withEnvironmentTypes k =
+  [ k (Proxy :: Proxy [Element])
+  , k (Proxy :: Proxy (Map Int Element))
+  , k (Proxy :: Proxy (Int, IntMap Element))
+  , k (Proxy :: Proxy (Int, HashMap Int Element))
+  , k (Proxy :: Proxy (Seq Element))
+  ]
+
+combinedBench :: forall env. (Environment env) => Int -> Proxy env -> Gauge.Benchmark
+combinedBench size _ =
+  Gauge.bench (name @env) $
+    Gauge.nf
+      (\size' -> 
+        let env = fromList @env $ replicate size' $ Element 41
+        in
+        [lookup env i | i <- [0..size - 1]]
+      )
+      size
+
+extensionBench :: forall env. (Environment env, NFData env) => Int -> Proxy env -> Gauge.Benchmark
+extensionBench size _ =
+  Gauge.bench (name @env) $ Gauge.nf (fromList @env) $ replicate size $ Element 41
+
+lookupBench :: forall env. (Environment env, NFData env) => Int -> Proxy env -> Gauge.Benchmark
+lookupBench size _ =
+  Gauge.env (pure $ fromList @env $ replicate size $ Element 41) $ \env ->
+    Gauge.bench (name @env) $
+      Gauge.nf (\size' -> [lookup env i | i <- [0..size' - 1]]) size
+
+main :: IO ()
+main =
+  Gauge.defaultMain
+    [ Gauge.bgroup "combined"
+      [Gauge.bgroup (show n) $ withEnvironmentTypes $ combinedBench n | n <- sizes]
+    , Gauge.bgroup "extension"
+      [Gauge.bgroup (show n) $ withEnvironmentTypes $ extensionBench n | n <- sizes]
+    , Gauge.bgroup "lookup"
+      [Gauge.bgroup (show n) $ withEnvironmentTypes $ lookupBench n | n <- sizes]
+    ]
+  where
+    sizes =
+      [1, 3, 5, 7, 10, 15, 20]
